e8b96c0a460839c0faeff0fad55676c48f223ca6
odeson <odeson@hotmail.com>
2016-04-10 21:53:00 +0200

    update README

--- javascript.js
+++ javascript.js
@@ -1,73 +1,14 @@
 
 var i=0;
-var k=16
+var k=16;
 
-$(function(){
-  var color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-  $("#content").on("mouseenter", ".square", function(){
-    $(this).css("background-color", color);
-  });
-    while(i <= k*k){
-    var size=768/k-4;
-    $(".square").css({"width": size, "height": size});
-    $("<div>").addClass("square").appendTo("#content");
-    i++;
-    }
-
-});
-
-$(function(){
-$("#resize").on("click", function() {
-  $(".square").remove();
-  var k = prompt("Please enter a new width:)");
-  i = 0;
-  var color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-  $("#content").on("mouseenter", ".square", function(){
-
-    $(this).css("background-color", color);
-  });
-  while(i <= k*k){
-    var size=768/k-4;
-  $(".square").css({"width": size, "height": size});
-  $("<div>").addClass("square").appendTo("#content");
-  i++;
+function applyColor(){
+	var color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+	$("#content").on("mouseenter", ".square", function(){
+    	$(this).css("background-color", color);	
+	});
 }
-});
-});
-$(function(){
-$("#w_button").on("click", function() {
-  var color = "white";
-  $("#content").on("mouseenter", ".square", function(){
 
-    $(this).css("background-color", color);
-  });
-});
-});
-$(function(){
-$("#clear").on("click", function() {
-  var color = "rgba(0,0,0,0.3)";
-  $(".square").css("background-color", color);
-
-});
-});
-$(function(){
-$("#random").on("click", function() {
-  var color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-  $("#content").on("mouseenter", ".square", function(){
-    $(this).css("background-color", color);
-  });
-
-});
-});
-$(function(){
-$("#rainbow").on("click", function() {
-  var color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-  $("#content").on("mouseenter", ".square", function(){
-    $(this).css("background-color", getRandomColor());
-  });
-
-});
-});
 function getRandomColor() {
     var letters = '0123456789ABCDEF'.split('');
     var color = '#';
@@ -76,3 +17,62 @@ function getRandomColor() {
     }
     return color;
 }
+
+function spawnSquares(){
+	while(i <= k*k){
+	var size=480/k-4;
+    $(".square").css({"width": size, "height": size});
+    $("<div>").addClass("square").appendTo("#content");
+    i++;
+	}
+}
+
+$(function(){
+  	applyColor();
+    spawnSquares();  
+});
+
+$(function(){
+ $("#resize").on("click", function() {
+  	$(".square").remove();
+  	i = 0;
+	applyColor();
+	var k = prompt("Please enter a new width:)");
+	while(i <= k*k){
+		var size=480/k-4;
+    	$(".square").css({"width": size, "height": size});
+    	$("<div>").addClass("square").appendTo("#content");
+   	 	i++;
+		}
+	});
+});
+
+$(function(){
+	$("#w_button").on("click", function() {
+  		var color = "white";
+  		$("#content").on("mouseenter", ".square", function(){
+    		$(this).css("background-color", color);
+  		});
+	});
+});
+
+$(function(){
+	$("#clear").on("click", function() {
+  		var color = "rgba(0,0,0,0.3)";
+  		$(".square").css("background-color", color);
+	});
+});
+
+$(function(){
+	$("#random").on("click", function() {
+  	applyColor();
+  	});
+});
+
+$(function(){
+	$("#rainbow").on("click", function() {
+  		$("#content").on("mouseenter", ".square", function(){
+   		 $(this).css("background-color", getRandomColor());
+ 		 });
+	});
+});
